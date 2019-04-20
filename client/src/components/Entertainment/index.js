709d898d60997fb06bc30dd6aba0fc15f228c805
Easter Bohanon <eastdabeast@email.arizona.edu>
2019-04-20 12:12:09 -0700

    don't play with my emotions

--- client/src/components/Entertainment/index.js
+++ client/src/components/Entertainment/index.js
@@ -1,8 +1,28 @@
-import React from "react";
-const Entertainment = () => {
-    return (
-        <h1>Entertainment Page</h1>
-    )
+import React, {Component} from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Header from "./Header/Header.js";
+import Musix from "./Musix.js";
+import Lyrics from './Lyrics/Lyrics.js';
+import { Provider } from './Context.js';
+
+
+class Entertainment extends Component {
+    render() {
+        return (
+            <Provider>
+                <Router>
+                    <React.Fragment>
+                        <div className="container">
+                            <Switch>
+                                <Route path="/entertainment" component={Musix} />
+                                <Route path="/lyrics/track/:id" component={Lyrics} />
+                            </Switch>
+                        </div>
+                    </React.Fragment>
+                </Router>
+            </Provider>
+        )
+    }
 }
 
 export default Entertainment;
